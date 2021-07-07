55e7ef53a18800c3671bbd46673e084a180dca59
playrey2010 <playrey2010@gmail.com>
2021-07-07 10:46:54 -0400

    My first commit, data type variables demo

---
+++ src/DataTypeVariables.java
@@ -0,0 +1,21 @@
+public class DataTypeVariables {
+    public static void main(String[] args){
+        boolean flag = true;
+        byte myByte = 100; // -132 - 133
+        char myChar = 'a';
+        short index = 2000;
+        int myInt = 3000000; // 3,000,000
+        float myFloat = 4.73F;
+        long longVar = 1234567890;  // 1,234,567,890
+        double doubleVar = 123456789.123456789;
+
+        System.out.println("flag:" + flag);
+        System.out.println("myByte: " + myByte);
+        System.out.println("myChar: " + myChar);
+        System.out.println("index: " + index);
+        System.out.println("myInt: " + myInt);
+        System.out.println("myFloat: " + myFloat);
+        System.out.println("longVar: " + longVar);
+        System.out.println("doubleVar: " + doubleVar);
+    }
+}
